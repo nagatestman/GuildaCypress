75fd65002eade4f129785499e3e4af78e9fe1b22
Renato Nagashima <renatonagashima@gmail.com>
2020-11-11 14:26:37 -0300

    fixtures

--- zero.webappsecurity/cypress/integration/fixtureuserData.spec.js
+++ zero.webappsecurity/cypress/integration/fixtureuserData.spec.js
@@ -2,7 +2,7 @@
 
 describe('Preenchendo um formulario com massa de dados json', () => {
 
-    it('Get data form tests', () => {
+    it('Get data form tests', function () {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.fixture('userData').as('usuario').then(() => {
             cy.get('#formNome').type(this.usuario.nome)
@@ -12,7 +12,7 @@ describe('Preenchendo um formulario com massa de dados json', () => {
             cy.get('#formEscolaridade').select(this.usuario.escolaridade)
             cy.get('#formEsportes').select(this.usuario.esportes)
             cy.get('#formCadastrar').click()
-            cy.get('#resultado > :nth-child(1)').should.('contain', 'Cadasrtado!')
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
         })
     })
 })
